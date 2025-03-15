34e35bc6dfa9c7af3f89e6bb134c43a919a6493f
Praveen <praveenprasad2510@gmail.com>
2025-03-15 14:41:27 +0530

    Login and Signup completed and working

--- backend_/server.js
+++ backend_/server.js
@@ -10,15 +10,12 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log(" ✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // Routes
 app.use("/auth", require("./Routes/auth"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
